771780e0278eeba1a0d77525314e4e304e582efe
Hoang Nhuoc Quy <hoangnhuocquy0@gmail.com>
2017-03-23 07:37:54 +0700

    ITO-000 update UserClient: getUserByEmail

--- src/main/java/onlinejudge/contest/client/UserClient.java
+++ src/main/java/onlinejudge/contest/client/UserClient.java
@@ -4,12 +4,18 @@ import java.util.Map;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import onlinejudge.domain.User;
 
 @FeignClient(name = "onlinejudge-ms-user")
 @RequestMapping("/onlinejudge-ms-user")
 public interface UserClient {
 	@RequestMapping("/get-user-id-by-email")
 	public @ResponseBody String getUserIDByEmail(@RequestParam Map<String, Object> param);
+	
+	@RequestMapping(value="/users", method=RequestMethod.GET)
+	public @ResponseBody User getUserByEmail(@RequestParam Map<String, Object> param);
 }
