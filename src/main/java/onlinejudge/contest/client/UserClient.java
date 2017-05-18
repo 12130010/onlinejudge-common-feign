c75d0daa403aed755b3df2a412a68b29d5f2fb11
Hoang Nhuoc Quy <hoangnhuocquy0@gmail.com>
2017-05-18 07:34:07 +0700

    ITO-000 new endpoint to get user

--- src/main/java/onlinejudge/contest/client/UserClient.java
+++ src/main/java/onlinejudge/contest/client/UserClient.java
@@ -3,6 +3,8 @@ package onlinejudge.contest.client;
 import java.util.Map;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,4 +20,7 @@ public interface UserClient {
 	
 	@RequestMapping(value="/users", method=RequestMethod.GET)
 	public @ResponseBody User getUserByEmail(@RequestParam Map<String, Object> param);
+	
+	@RequestMapping(value="/get-user-by-email", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody User getUserByEmail2(@RequestParam Map<String, Object> param);
 }
